22c0fd84d888fb51ee15e76bbdd1cad85fd86c84
Jose <j2figuer@gmail.com>
2024-12-18 17:29:34 -0800

    added the import_ppd function with credit to Thomas Akam's Github

--- src/jdb_to_nwb/convert_photometry.py
+++ src/jdb_to_nwb/convert_photometry.py
@@ -2,6 +2,8 @@ from pynwb import NWBFile
 import struct
 import pandas as pd
 import numpy as np
+import os
+import json
 import warnings
 import scipy.io
 from scipy.signal import butter, lfilter, hilbert, filtfilt
@@ -336,17 +338,13 @@ def airPLS(data, lambda_=1e8, max_iterations=50):
         weights[-1] = weights[0]
     return baseline
 
-def processppdphotometry(ppd_file_path):
-    """
-    Process ppd file from pyPhotometry into a dict
-    requires the import_ppd function from the data_import.py script
-    The code needed is below.
+def import_ppd(ppd_file_path):
+    '''
+    Credit to the homie: https://github.com/ThomasAkam/photometry_preprocessing.git
+    I eddited it so that his function only retuns the data dictionary without the filtered data.
+    Raw data is filtered later/separately using the processppdphotometry function.
 
-        def import_ppd(file_path, low_pass=20, high_pass=15.9):
-        Function to import pyPhotometry binary data files into Python. The high_pass
-        and low_pass arguments determine the frequency in Hz of highpass and lowpass
-        filtering applied to the filtered analog signals. To disable highpass or lowpass
-        filtering set the respective argument to None.  Returns a dictionary with the
+        Function to import pyPhotometry binary data files into Python. Returns a dictionary with the
         following items:
             'filename'      - Data filename
             'subject_ID'    - Subject ID
@@ -359,9 +357,6 @@ def processppdphotometry(ppd_file_path):
             'analog_1'      - Raw analog signal 1 (volts)
             'analog_2'      - Raw analog signal 2 (volts)
             'analog_3'      - Raw analog signal 3 (if present, volts)
-            'analog_1_filt' - Filtered analog signal 1 (volts)
-            'analog_2_filt' - Filtered analog signal 2 (volts)
-            'analog_3_filt' - Filtered analog signal 2 (if present, volts)
             'digital_1'     - Digital signal 1
             'digital_2'     - Digital signal 2 (if present)
             'pulse_inds_1'  - Locations of rising edges on digital input 1 (samples).
@@ -369,8 +364,8 @@ def processppdphotometry(ppd_file_path):
             'pulse_times_1' - Times of rising edges on digital input 1 (ms).
             'pulse_times_2' - Times of rising edges on digital input 2 (ms).
             'time'          - Time of each sample relative to start of recording (ms)
-
-    with open(file_path, "rb") as f:
+    '''
+    with open(, "rb") as f:
         header_size = int.from_bytes(f.read(2), "little")
         data_header = f.read(header_size)
         data = np.frombuffer(f.read(), dtype=np.dtype("<u2"))
@@ -394,19 +389,7 @@ def processppdphotometry(ppd_file_path):
     digital_1 = digital[::n_analog_signals]
     digital_2 = digital[1::n_analog_signals] if n_digital_signals == 2 else None
     time = np.arange(analog_1.shape[0]) * 1000 / sampling_rate  # Time relative to start of recording (ms).
-    # Filter signals with specified high and low pass frequencies (Hz).
-    if low_pass and high_pass:
-        b, a = butter(2, np.array([high_pass, low_pass]) / (0.5 * sampling_rate), "bandpass")
-    elif low_pass:
-        b, a = butter(2, low_pass / (0.5 * sampling_rate), "low")
-    elif high_pass:
-        b, a = butter(2, high_pass / (0.5 * sampling_rate), "high")
-    if low_pass or high_pass:
-        analog_1_filt = filtfilt(b, a, analog_1)
-        analog_2_filt = filtfilt(b, a, analog_2)
-        analog_3_filt = filtfilt(b, a, analog_3) if n_analog_signals == 3 else None
-    else:
-        analog_1_filt = analog_2_filt = analog_3_filt = None
+    
     # Extract rising edges for digital inputs.
     pulse_inds_1 = 1 + np.where(np.diff(digital_1) == 1)[0]
     pulse_inds_2 = 1 + np.where(np.diff(digital_2) == 1)[0] if n_digital_signals == 2 else None
@@ -414,11 +397,9 @@ def processppdphotometry(ppd_file_path):
     pulse_times_2 = pulse_inds_2 * 1000 / sampling_rate if n_digital_signals == 2 else None
     # Return signals + header information as a dictionary.
     data_dict = {
-        "filename": os.path.basename(file_path),
+        "filename": os.path.basename(ppd_file_path),
         "analog_1": analog_1,
         "analog_2": analog_2,
-        "analog_1_filt": analog_1_filt,
-        "analog_2_filt": analog_2_filt,
         "digital_1": digital_1,
         "digital_2": digital_2,
         "pulse_inds_1": pulse_inds_1,
@@ -431,11 +412,14 @@ def processppdphotometry(ppd_file_path):
         data_dict.update(
             {
                 "analog_3": analog_3,
-                "analog_3_filt": analog_3_filt,
             }
         )
     data_dict.update(header_dict)
     return data_dict
+
+def processppdphotometry(ppd_file_path):
+    """
+        
     """
     ppd_data = import_ppd(ppd_file_path)  
 
