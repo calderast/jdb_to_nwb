d7b66f3aaa0dcf4f7e935c88c25bb3f11a4f9fa8
Stephanie Crater <70605721+calderast@users.noreply.github.com>
2025-01-22 12:11:07 -0800

    Reorganize photometry code, add tests for pyphotometry (#41)
    * Reorganize photometry code to separate labview and pyphotometry
    * Update tests for pyphotometry
    * Update download script to download pyphotometry test data
    * remove unused photometry imports and shorten long lines to make ruff happy

--- tests/test_convert_photometry.py
+++ tests/test_convert_photometry.py
@@ -7,11 +7,14 @@ from dateutil import tz
 from pynwb import NWBFile
 from ndx_fiber_photometry import FiberPhotometryResponseSeries
 
-from jdb_to_nwb.convert_photometry import add_photometry, process_raw_photometry_signals
+from jdb_to_nwb.convert_photometry import add_photometry, process_raw_labview_photometry_signals
 
 
-def test_process_raw_photometry_signals():
-    """Test that the process_raw_photometry_signals function returns a signals dictionary equivalent to signals.mat."""
+def test_process_raw_labview_photometry_signals():
+    """
+    Test that the process_raw_labview_photometry_signals function 
+    returns a signals dictionary equivalent to signals.mat.
+    """
 
     # Create a test metadata dictionary
     test_data_dir = Path("tests/test_data/downloaded/IM-1478/07252022")
@@ -28,7 +31,7 @@ def test_process_raw_photometry_signals():
     # Process the raw .phot and .box files from Labview into an equivalent signals dict
     phot_file_path = metadata["photometry"]["phot_file_path"]
     box_file_path = metadata["photometry"]["box_file_path"]
-    signals = process_raw_photometry_signals(phot_file_path, box_file_path)
+    signals = process_raw_labview_photometry_signals(phot_file_path, box_file_path)
 
     # Ensure all relevant keys are present in both the reference and created signals dict
     expected_keys = {"sig1", "sig2", "loc", "ref", "visits"}
@@ -47,23 +50,37 @@ def test_process_raw_photometry_signals():
     # In our former MATLAB photometry preprocessing pipeline (used to create signals.mat), we would sometimes
     # remove a certain number of samples from the beginning of our signals for alignment with behavior.
     # In this new pipeline, we do this alignment later so this is no longer necessary at this step.
-    # This results in the signals in our created signals dict being longer than the signals in the reference signals.mat,
+    # This results in the signals in our created dict being longer than the reference signals in signals.mat,
     # so we calculate the number of samples to remove from the beginning so we can do comparisons.
     samples_removed_from_reference = len(signals["ref"]) - len(reference_signals_mat["ref"].flatten())
 
-    # The difference between our created signal and the reference from signals.mat must be <0.01% of the magnitude of the reference
-    assert np.allclose(signals["ref"][samples_removed_from_reference:], reference_signals_mat["ref"].flatten(), rtol=1e-4)
-    assert np.allclose(signals["sig1"][samples_removed_from_reference:], reference_signals_mat["sig1"].flatten(), rtol=1e-4)
-    # We allow an additional absolute tolerance for sig2 because there are some values very close to 0 where relative tolerance is less useful
-    # (Also, we don't use actually sig2 so it doesn't really matter)
-    assert np.allclose(signals["sig2"][samples_removed_from_reference:], reference_signals_mat["sig2"].flatten(), rtol=1e-4, atol=5)
+    # The difference between our signal and the reference must be <0.01% of the magnitude of the reference
+    assert np.allclose(
+        signals["ref"][samples_removed_from_reference:], 
+        reference_signals_mat["ref"].flatten(), 
+        rtol=1e-4
+    )
+    assert np.allclose(
+        signals["sig1"][samples_removed_from_reference:], 
+        reference_signals_mat["sig1"].flatten(), 
+        rtol=1e-4
+    )
+    # We allow an additional absolute tolerance for sig2 because there are some values very close to 0 
+    # where relative tolerance is less useful. (Also, we don't use actually sig2 so it doesn't really matter)
+    assert np.allclose(
+        signals["sig2"][samples_removed_from_reference:], 
+        reference_signals_mat["sig2"].flatten(), 
+        rtol=1e-4, 
+        atol=5
+    )
 
 
 def test_add_photometry_from_signals_mat():
     """
     Test that the add_photometry function results in the expected FiberPhotometryResponseSeries.
 
-    This version of the test uses the already created signals.mat at "signals_mat_file_path" to further process and add photometry signals to the NWB.
+    This version of the test uses the already created signals.mat at "signals_mat_file_path" 
+    to further process and add photometry signals to the NWB.
     """
 
     # Create a test metadata dictionary with preprocessed LabVIEW data ("signals_mat_file_path")
@@ -84,11 +101,15 @@ def test_add_photometry_from_signals_mat():
     )
 
     # Add photometry data to the nwbfile
-    visits = add_photometry(nwbfile=nwbfile, metadata=metadata)
+    sampling_rate, visits = add_photometry(nwbfile=nwbfile, metadata=metadata)
 
     # Define the FiberPhotometryResponseSeries we expect to have been added to the nwbfile
     expected_photometry_series = {"raw_green", "raw_reference", "z_scored_green_dFF", "z_scored_reference_fitted"}
     expected_sampling_rate = 250  # Hz
+
+    # Assert that we have returned the correct sampling rate
+    assert sampling_rate == expected_sampling_rate, (
+        f"Expected sampling rate {expected_sampling_rate} Hz, got {sampling_rate} Hz")
 
     # Assert that all expected photometry series are present in the acquisition field of the nwbfile
     actual_photometry_series = set(nwbfile.acquisition.keys())
@@ -98,11 +119,15 @@ def test_add_photometry_from_signals_mat():
     # Check the basic attributes of each series
     for series_name in expected_photometry_series:
         # Assert that all series are of type FiberPhotometryResponseSeries
-        assert isinstance(nwbfile.acquisition[series_name], FiberPhotometryResponseSeries), f"{series_name} is not of type FiberPhotometryResponseSeries"
+        assert isinstance(nwbfile.acquisition[series_name], FiberPhotometryResponseSeries), (
+            f"{series_name} is not of type FiberPhotometryResponseSeries")
         # Assert all series have a sampling rate of 250 Hz
         assert (
             getattr(nwbfile.acquisition[series_name], "rate", None) == expected_sampling_rate
-        ), f"{series_name} has a sampling rate of {getattr(nwbfile.acquisition[series_name], 'rate', None)}, expected {expected_sampling_rate}"
+        ), (
+            f"{series_name} has a sampling rate of {getattr(nwbfile.acquisition[series_name], 'rate', None)}, "
+            f"expected {expected_sampling_rate}"
+        )
 
     # Check that the photometry series in the nwbfile match the expected signals from the reference dataframe
     green_z_scored_dFF = np.array(nwbfile.acquisition["z_scored_green_dFF"].data)
@@ -151,11 +176,15 @@ def test_add_photometry_from_raw_labview():
     )
 
     # Add photometry data to the nwbfile
-    visits = add_photometry(nwbfile=nwbfile, metadata=metadata)
+    sampling_rate, visits = add_photometry(nwbfile=nwbfile, metadata=metadata)
 
     # Define the FiberPhotometryResponseSeries we expect to have been added to the nwbfile
     expected_photometry_series = {"raw_green", "raw_reference", "z_scored_green_dFF", "z_scored_reference_fitted"}
     expected_sampling_rate = 250  # Hz
+
+    # Assert that we have returned the correct sampling rate
+    assert sampling_rate == expected_sampling_rate, (
+        f"Expected sampling rate {expected_sampling_rate} Hz, got {sampling_rate} Hz")
 
     # Assert that all expected photometry series are present in the acquisition field of the nwbfile
     actual_photometry_series = set(nwbfile.acquisition.keys())
@@ -165,20 +194,24 @@ def test_add_photometry_from_raw_labview():
     # Check the basic attributes of each series
     for series_name in expected_photometry_series:
         # Assert that all series are of type FiberPhotometryResponseSeries
-        assert isinstance(nwbfile.acquisition[series_name], FiberPhotometryResponseSeries), f"{series_name} is not of type FiberPhotometryResponseSeries"
+        assert isinstance(nwbfile.acquisition[series_name], FiberPhotometryResponseSeries), (
+            f"{series_name} is not of type FiberPhotometryResponseSeries")
         # Assert all series have a sampling rate of 250 Hz
         assert (
             getattr(nwbfile.acquisition[series_name], "rate", None) == expected_sampling_rate
-        ), f"{series_name} has a sampling rate of {getattr(nwbfile.acquisition[series_name], 'rate', None)}, expected {expected_sampling_rate}"
+        ), (
+            f"{series_name} has a sampling rate of {getattr(nwbfile.acquisition[series_name], 'rate', None)}, "
+            f"expected {expected_sampling_rate}"
+        )
 
     # Check that the photometry series in the nwbfile match the expected signals from the reference dataframe
     green_z_scored_dFF = np.array(nwbfile.acquisition["z_scored_green_dFF"].data)
     green_z_scored_dFF_reference = reference_dataframe["green"].values
 
-    # In our former MATLAB photometry preprocessing pipeline (used to create the reference dataframe), we would sometimes
-    # remove a certain number of samples from the beginning of our signals for alignment with behavior.
+    # In our former MATLAB photometry preprocessing pipeline (used to create the reference dataframe), 
+    # we would sometimes remove some samples from the beginning of our signals for alignment with behavior.
     # In this new pipeline, we do this alignment later so this is no longer necessary at this step.
-    # This results in the signals in our created signals dict being longer than the signals in the reference dataframe,
+    # This results in the signals in our created dict being longer than the reference signals in the dataframe,
     # so we calculate the number of samples to remove from the beginning so we can do comparisons.
     samples_removed_from_reference = len(green_z_scored_dFF) - len(green_z_scored_dFF_reference)
     green_z_scored_dFF = green_z_scored_dFF[samples_removed_from_reference:]
@@ -204,33 +237,80 @@ def test_add_photometry_from_pyphotometry():
     Test that the add_photometry function results in the expected FiberPhotometryResponseSeries.
 
     This version of the test uses the ppd file from pyPhotometry to add photometry signals to the NWB.
-    
-    TODO: Implement this test once adding photometry via ppd file is implemented!!
     """
 
     # Create a test metadata dictionary with pyPhotometry data
+    test_data_dir = Path("tests/test_data/downloaded/IM-1770_corvette/11062024")
     metadata = {}
     metadata["photometry"] = {}
-    metadata["photometry"]["ppd_file_path"] = "" # TODO. 
+    metadata["photometry"]["ppd_file_path"] = test_data_dir / "Lhem_barswitch_GACh4h_rDA3m_CKTL-2024-11-06-185407.ppd"
 
+    # Define paths to reference data
+    reference_data_path = test_data_dir / "sampleframe.csv"
+    reference_dataframe = pd.read_csv(reference_data_path)
+    
     # Create a test NWBFile
     nwbfile = NWBFile(
         session_description="Mock session",
         session_start_time=datetime.now(tz.tzlocal()),
         identifier="mock_session",
     )
-    
-    # TODO: Actually implement this test.
-    # Reference data and most assertions can likely be copied from the above tests.
-    # For now we test that the appropriate NotImplementedError is raised.
 
-    # Check that add_photometry complains about pyPhotometry not being implemented if we specify a ppd file
-    try:
-        visits = add_photometry(nwbfile=nwbfile, metadata=metadata)
-    except NotImplementedError as e:
-        assert str(e) == "pyPhotometry processing is not yet implemented."
-    else:
-        assert False, "Expected NotImplementedError when attempting to process pyPhotometry data."
+    # Add photometry data to the nwbfile
+    sampling_rate, visits = add_photometry(nwbfile=nwbfile, metadata=metadata)
+
+    # Define the FiberPhotometryResponseSeries we expect to have been added to the nwbfile
+    expected_photometry_series = {"raw_470", "z_scored_470", "raw_405", "zscored_405", "raw_565", 
+                                  "zscored_565", "raw_470_405_ratio", "zscored_470_405_ratio"}
+    expected_sampling_rate = 86 # Hz
+
+    # Assert that we have returned the correct sampling rate
+    assert sampling_rate == expected_sampling_rate, (
+        f"Expected sampling rate {expected_sampling_rate} Hz, got {sampling_rate} Hz")
+
+    # Assert that all expected photometry series are present in the acquisition field of the nwbfile
+    actual_photometry_series = set(nwbfile.acquisition.keys())
+    missing_photometry_series = expected_photometry_series - actual_photometry_series
+    assert not missing_photometry_series, f"Missing FiberPhotometryResponseSeries: {missing_photometry_series}"
+
+    # Check the basic attributes of each series
+    for series_name in expected_photometry_series:
+        # Assert that all series are of type FiberPhotometryResponseSeries
+        assert isinstance(nwbfile.acquisition[series_name], FiberPhotometryResponseSeries), (
+            f"{series_name} is not of type FiberPhotometryResponseSeries")
+        # Assert all series have a sampling rate of 86 Hz
+        assert (
+            getattr(nwbfile.acquisition[series_name], "rate", None) == expected_sampling_rate
+        ), (
+            f"{series_name} has a sampling rate of {getattr(nwbfile.acquisition[series_name], 'rate', None)}, "
+            f"expected {expected_sampling_rate}"
+        )
+        
+    # Check that the photometry series in the nwbfile match the expected signals from the reference dataframe
+    nwb_signal_names = ["raw_470", "z_scored_470", "raw_405", "zscored_405", 
+                        "raw_565", "zscored_565", "raw_470_405_ratio", "zscored_470_405_ratio"]
+    reference_signal_names = ["raw_green", "green_z_scored", "raw_405", "z_scored_405", 
+                              "raw_red", "red_z_scored", "raw 470/405", "ratio_z_scored"]
+
+    # Compare each signal in the nwbfile to its respective reference
+    for sig_name, ref_name in zip(nwb_signal_names, reference_signal_names):
+        signal = np.array(nwbfile.acquisition[sig_name].data)
+        reference = reference_dataframe[ref_name].values
+    
+        # Check that the lengths match
+        assert len(signal) == len(reference), (
+            f"Data length mismatch: {sig_name} has {len(signal)} points, "
+            f"but the reference signal {ref_name} has {len(reference)} points."
+        )
+
+        # Check that the signal in the nwbfile matches the reference signal (within a tolerance)
+        np.testing.assert_allclose(
+            signal,
+            reference,
+            atol=0.005,
+            rtol=0.05,
+            err_msg=f"Data mismatch between nwbfile {sig_name} and reference {ref_name}",
+        )
 
 
 def test_add_photometry_with_incomplete_metadata(capsys):
@@ -260,11 +340,12 @@ def test_add_photometry_with_incomplete_metadata(capsys):
     # valid way to specify that we have no photometry data for this session.
     
     # Call the add_photometry function with no 'photometry' key in metadata
-    visits = add_photometry(nwbfile=nwbfile, metadata=metadata)
+    sampling_rate, visits = add_photometry(nwbfile=nwbfile, metadata=metadata)
     captured = capsys.readouterr() # capture stdout
     
-    # Check that the correct message was printed to stdout and returned visits is None
+    # Check that the correct message was printed to stdout and returned sampling_rate and visits is None
     assert "No photometry metadata found for this session. Skipping photometry conversion." in captured.out
+    assert sampling_rate is None
     assert visits is None
     
     # Create a test metadata dictionary with a photometry field but no photometry data
@@ -272,9 +353,11 @@ def test_add_photometry_with_incomplete_metadata(capsys):
     
     # Check that add_photometry raises a ValueError about missing fields in the metadata dictionary
     try:
-        visits = add_photometry(nwbfile=nwbfile, metadata=metadata)
+        sampling_rate, visits = add_photometry(nwbfile=nwbfile, metadata=metadata)
     except ValueError as e:
         assert str(e).startswith("None of the required photometry subfields exist in the metadata dictionary")
     else:
-        assert False, "Expected ValueError was not raised in response to missing photometry subfields in the metadata dict."
-        
+        assert False, (
+            "Expected ValueError was not raised in response to "
+            "missing photometry subfields in the metadata dict."
+        )
