83ea169f0960591f4978098bb83e04c30e81f824
Stephanie Crater <stephcrater@berkeley.edu>
2025-01-22 07:52:05 -0800

    remove deprecated np.matrix

--- src/jdb_to_nwb/convert_photometry.py
+++ src/jdb_to_nwb/convert_photometry.py
@@ -274,7 +274,7 @@ def whittaker_smooth(data, binary_mask, lambda_):
     the fitted background vector
     """
 
-    data_matrix = np.matrix(data)
+    data_matrix = np.array(data)
     data_size = data_matrix.size  # Size of the data matrix
     # Create an identity matrix the size of the data matrix in compressed sparse column (csc) format
     identity_matrix = eye(data_size, format="csc")
