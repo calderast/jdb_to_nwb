cfd10786792836e9679491c150432d8e2fb9d57b
Stephanie Crater <stephcrater@berkeley.edu>
2025-01-21 22:05:09 -0800

    Update Berke lab behavior processing for consistency with Frank lab

--- tests/test_convert_behavior.py
+++ tests/test_convert_behavior.py
@@ -39,44 +39,28 @@ def test_convert_behavior():
 
     # Test that the session description has been updated
     assert (
-        nwbfile.experiment_description == "Barrier change session for the hex maze task with 3 blocks and 188 trials."
+        nwbfile.experiment_description == "barrier change session for the hex maze task with 3 blocks and 188 trials."
     )
 
     # Test that expected block data has been added to the nwbfile
     assert "block" in nwbfile.intervals
     expected_block_columns = {
-        "start_time",
-        "stop_time",
-        "block",
-        "maze_configuration",
-        "prob_A",
-        "prob_B",
-        "prob_C",
-        "num_trials_in_block",
-    }
-    assert set(nwbfile.intervals["block"].colnames) == expected_block_columns
-    assert len(nwbfile.intervals["block"].start_time) == 3  # there are 3 blocks in this session
+        'start_time', 'stop_time', 'epoch', 'block', 
+        'maze_configuration', 'pA', 'pB', 'pC', 'num_trials', 'task_type'}
+    assert set(nwbfile.intervals["block"].colnames) == expected_block_columns, f"Block columns {nwbfile.intervals['block'].colnames} did not match expected"
+    assert len(nwbfile.intervals["block"].start_time) == 3 # there are 3 blocks in this session
     # Check block data
-    assert nwbfile.intervals["block"].num_trials_in_block[0] == 68
-    assert nwbfile.intervals["block"].maze_configuration[0] == "{35, 11, 12, 45, 14, 15, 18, 22, 29, 31}"
-    assert nwbfile.intervals["block"].prob_A[0] == 10
-    assert nwbfile.intervals["block"].prob_B[0] == 50
-    assert nwbfile.intervals["block"].prob_C[0] == 90
+    assert nwbfile.intervals["block"].num_trials[0] == 68
+    assert nwbfile.intervals["block"]["maze_configuration"][0] == "11,12,14,15,18,22,29,31,35,45", f"Maze configuration {nwbfile.intervals['block']['maze_configuration'][0]} did not match expected"
+    assert nwbfile.intervals["block"].pA[0] == 10
+    assert nwbfile.intervals["block"].pB[0] == 50
+    assert nwbfile.intervals["block"].pC[0] == 90
 
     # Test that expected trial data has been added to the nwbfile
     assert nwbfile.trials is not None
     expected_trial_columns = {
-        "start_time",
-        "stop_time",
-        "duration",
-        "trial",
-        "trial_within_session",
-        "block",
-        "start_port",
-        "end_port",
-        "reward",
-        "beam_break_start",
-        "beam_break_end",
-    }
-    assert set(nwbfile.trials.colnames) == expected_trial_columns
-    assert len(nwbfile.trials.start_time) == 188  # there are 188 trials in this session
+        'start_time', 'stop_time', 'epoch', 'block', 'trial_within_block', 
+        'trial_within_epoch', 'start_port', 'end_port', 'reward', 
+        'opto_condition', 'duration', 'poke_in', 'poke_out'}
+    assert set(nwbfile.trials.colnames) == expected_trial_columns, f"Trial columns {nwbfile.trials.colnames} did not match expected"
+    assert len(nwbfile.trials.start_time) == 188 # there are 188 trials in this session
