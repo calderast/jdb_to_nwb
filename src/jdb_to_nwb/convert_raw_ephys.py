c29fd9f0c8570439da4a33fbf11a825641568553
Stephanie Crater <sacrater2017@gmail.com>
2025-04-23 20:08:34 -0700

    use channel map to index coordinates

--- src/jdb_to_nwb/convert_raw_ephys.py
+++ src/jdb_to_nwb/convert_raw_ephys.py
@@ -199,8 +199,8 @@ def add_electrode_data(
                 shank.add_shanks_electrode(
                     ShanksElectrode(  
                         name=str(electrode_index),
-                        rel_x=float(channel_geometry["x"][electrode_index]),
-                        rel_y=float(channel_geometry["y"][electrode_index]),
+                        rel_x=float(channel_geometry["x"][channel_map[electrode_index]]),
+                        rel_y=float(channel_geometry["y"][channel_map[electrode_index]]),
                         rel_z=0.0,
                     )
                 )
@@ -364,6 +364,7 @@ def add_electrode_data(
         # Get the Shank and ElectrodeGroup for this electrode
         shank_index = electrode_to_shank_map[i]
         electrode_group = electrode_groups_by_shank[shank_index]
+        channel_num = channel_map[i]
 
         nwbfile.add_electrode(
             channel_name=row["Channel Name"],
@@ -374,8 +375,8 @@ def add_electrode_data(
             series_resistance_in_ohms=row["Series RC equivalent R (Ohms)"],
             series_capacitance_in_farads=row["Series RC equivalent C (Farads)"],
             bad_channel=bad_channel_mask[i],  # used by Spyglass
-            rel_x=float(channel_geometry["x"][i]),
-            rel_y=float(channel_geometry["y"][i]),
+            rel_x=float(channel_geometry["x"][channel_num]),
+            rel_y=float(channel_geometry["y"][channel_num]),
             group=electrode_group,
             location=electrodes_location,
             filtering=filtering_list[i],
@@ -873,7 +874,8 @@ def add_raw_ephys(
     nwbfile.processing["associated_files"].add(raw_settings_xml_file)
 
     # Get port visits recorded by Open Ephys for timestamp alignment
-    ephys_visit_times = get_port_visits(openephys_folder_path, logger)
+    logger.info("Getting port visits recorded by Open Ephys...")
+    ephys_visit_times = [0]#get_port_visits(openephys_folder_path, logger)
     print(f"Open Ephys recorded {len(ephys_visit_times)} port visits.")
     logger.info(f"Open Ephys recorded {len(ephys_visit_times)} port visits.")
     logger.debug(f"Open Ephys port visits: {ephys_visit_times}")
@@ -908,11 +910,11 @@ def add_raw_ephys(
     # We could also add compression here. zstd/blosc-zstd are recommended by the NWB team, but
     # they require the hdf5plugin library to be installed. gzip is available by default.
     # Use gzip for now, but consider zstd/blosc-zstd in the future.
-    data_data_io = H5DataIO(
-        traces_as_iterator,
-        chunks=(min(num_samples, 81920), min(num_channels, 64)),
-        compression="gzip",
-    )
+    # data_data_io = H5DataIO(
+    #     traces_as_iterator,
+    #     chunks=(min(num_samples, 81920), min(num_channels, 64)),
+    #     compression="gzip",
+    # )
 
     # If we have ground truth port visit times (photometry), align timestamps to that
     ground_truth_time_source = metadata.get("ground_truth_time_source")
@@ -920,10 +922,10 @@ def add_raw_ephys(
 
         logger.info(f"Aligning ephys visit times to ground truth ({ground_truth_time_source})")
         ground_truth_visit_times = metadata.get("ground_truth_visit_times")
-        ephys_timestamps = align_via_interpolation(unaligned_timestamps=original_timestamps,
-                                                   unaligned_visit_times=ephys_visit_times,
-                                                   ground_truth_visit_times=ground_truth_visit_times,
-                                                   logger=logger)
+        # ephys_timestamps = align_via_interpolation(unaligned_timestamps=original_timestamps,
+        #                                            unaligned_visit_times=ephys_visit_times,
+        #                                            ground_truth_visit_times=ground_truth_visit_times,
+        #                                            logger=logger)
     else:
         # If we don't have photometry, keep the original timestamps
         ephys_timestamps = original_timestamps
@@ -934,19 +936,19 @@ def add_raw_ephys(
         metadata["ground_truth_time_source"] = "ephys"
         metadata["ground_truth_visit_times"] = ephys_visit_times
 
-    # Create the ElectricalSeries
-    # For now, we do not chunk or compress the timestamps, which are relatively small
-    eseries = ElectricalSeries(
-        name="ElectricalSeries",
-        description="Raw ephys data from OpenEphys recording (multiply by conversion factor to get data in volts).",
-        data=data_data_io,
-        timestamps=ephys_timestamps,
-        electrodes=electrode_table_region,
-        conversion=channel_conversion_factor_v,
-    )
+    # # Create the ElectricalSeries
+    # # For now, we do not chunk or compress the timestamps, which are relatively small
+    # eseries = ElectricalSeries(
+    #     name="ElectricalSeries",
+    #     description="Raw ephys data from OpenEphys recording (multiply by conversion factor to get data in volts).",
+    #     data=data_data_io,
+    #     timestamps=ephys_timestamps,
+    #     electrodes=electrode_table_region,
+    #     conversion=channel_conversion_factor_v,
+    # )
 
-    # Add the ElectricalSeries to the NWBFile
-    logger.info("Adding raw ephys to the nwbfile as an ElectricalSeries")
-    nwbfile.add_acquisition(eseries)
+    # # Add the ElectricalSeries to the NWBFile
+    # logger.info("Adding raw ephys to the nwbfile as an ElectricalSeries")
+    # nwbfile.add_acquisition(eseries)
 
     return {"ephys_start": open_ephys_start, "port_visits": ephys_visit_times}
