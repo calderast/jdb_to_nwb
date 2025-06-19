004a9b34a44390971e367f594c14b66d666f1761
rly <rly@lbl.gov>
2025-06-18 23:53:07 -0700

    Update some tests, more to go

--- tests/test_convert_photometry.py
+++ tests/test_convert_photometry.py
@@ -12,27 +12,41 @@ from jdb_to_nwb.convert_photometry import add_photometry, process_raw_labview_ph
 
 def add_dummy_photometry_metadata_to_metadata(metadata):
     """
-    Add dummy values to the metadata dictionary so that the tests can run. These values are not tested.
+    Add dummy values to the metadata dictionary so that the tests can run.
     test_add_photometry_metadata tests that the values are added correctly.
     """
-
-    metadata["photometry"]["excitation_sources"] = [
-        "Thorlabs Purple LED",
-    ]
     metadata["photometry"]["optic_fibers"] = [
         "Doric 0.66mm Flat 40mm Optic Fiber",
     ]
-    metadata["photometry"]["photodetectors"] = [
+    metadata["photometry"]["photodetector"] = [
         "Doric iFMC7-G2 (7 ports Fluorescence Mini Cube - Three Fluorophores)",
     ]
-    # metadata["photometry"]["optic_fiber_implant_sites"] = []
-    # metadata["photometry"]["viruses"] = []
-    # metadata["photometry"]["virus_injections"] = []
+    metadata["photometry"]["optic_fiber_implant_sites"] = [
+        {
+            "optic_fiber": "Doric 0.66mm Flat 40mm Optic Fiber",
+            "targeted_location": "NAcc",
+            "ap_in_mm": 1.7,
+            "ml_in_mm": 1.7,
+            "dv_in_mm": -6.0,
+            "recording": True,
+        }
+    ]
+    metadata["photometry"]["virus_injections"] = [
+        {
+            "virus_name": "dLight1.3b",
+            "targeted_location": "NAcc",
+            "ap_in_mm": 1.7,
+            "ml_in_mm": 1.7,
+            "dv_in_mm": -6.2,
+            "volume_in_uL": 1.0,
+            "titer_in_vg_per_ml": 2e12,
+        }
+    ]
 
 
 def test_process_raw_labview_photometry_signals(dummy_logger):
     """
-    Test that the process_raw_labview_photometry_signals function 
+    Test that the process_raw_labview_photometry_signals function
     returns a signals dictionary equivalent to signals.mat.
     """
 
@@ -76,21 +90,21 @@ def test_process_raw_labview_photometry_signals(dummy_logger):
 
     # The difference between our signal and the reference must be <0.01% of the magnitude of the reference
     assert np.allclose(
-        signals["ref"][samples_removed_from_reference:], 
-        reference_signals_mat["ref"].flatten(), 
+        signals["ref"][samples_removed_from_reference:],
+        reference_signals_mat["ref"].flatten(),
         rtol=1e-4
     )
     assert np.allclose(
-        signals["sig1"][samples_removed_from_reference:], 
-        reference_signals_mat["sig1"].flatten(), 
+        signals["sig1"][samples_removed_from_reference:],
+        reference_signals_mat["sig1"].flatten(),
         rtol=1e-4
     )
-    # We allow an additional absolute tolerance for sig2 because there are some values very close to 0 
+    # We allow an additional absolute tolerance for sig2 because there are some values very close to 0
     # where relative tolerance is less useful. (Also, we don't use actually sig2 so it doesn't really matter)
     assert np.allclose(
-        signals["sig2"][samples_removed_from_reference:], 
-        reference_signals_mat["sig2"].flatten(), 
-        rtol=1e-4, 
+        signals["sig2"][samples_removed_from_reference:],
+        reference_signals_mat["sig2"].flatten(),
+        rtol=1e-4,
         atol=5
     )
 
@@ -99,7 +113,7 @@ def test_add_photometry_from_signals_mat(dummy_logger):
     """
     Test that the add_photometry function results in the expected FiberPhotometryResponseSeries.
 
-    This version of the test uses the already created signals.mat at "signals_mat_file_path" 
+    This version of the test uses the already created signals.mat at "signals_mat_file_path"
     to further process and add photometry signals to the NWB.
     """
 
@@ -108,6 +122,10 @@ def test_add_photometry_from_signals_mat(dummy_logger):
     metadata = {}
     metadata["photometry"] = {}
     metadata["photometry"]["signals_mat_file_path"] = test_data_dir / "signals.mat"
+    metadata["photometry"]["excitation_sources"] = [
+        "Thorlabs Purple LED",
+        "Thorlabs Blue LED",
+    ]
     add_dummy_photometry_metadata_to_metadata(metadata)
 
     # Define paths to reference data
@@ -182,7 +200,7 @@ def test_add_photometry_from_raw_labview(dummy_logger):
     """
     Test that the add_photometry function results in the expected FiberPhotometryResponseSeries.
 
-    This version of the test uses the raw LabVIEW data at "phot_file_path" and "box_file_path" 
+    This version of the test uses the raw LabVIEW data at "phot_file_path" and "box_file_path"
     to first create a signals dict, and then further process and add photometry signals to the NWB.
     """
 
@@ -192,6 +210,10 @@ def test_add_photometry_from_raw_labview(dummy_logger):
     metadata["photometry"] = {}
     metadata["photometry"]["phot_file_path"] = test_data_dir / "IM-1478_2022-07-25_15-24-22____Tim_Conditioning.phot"
     metadata["photometry"]["box_file_path"] = test_data_dir / "IM-1478_2022-07-25_15-24-22____Tim_Conditioning.box"
+    metadata["photometry"]["excitation_sources"] = [
+        "Thorlabs Purple LED",
+        "Thorlabs Blue LED",
+    ]
     add_dummy_photometry_metadata_to_metadata(metadata)
 
     # Define paths to reference data
@@ -246,7 +268,7 @@ def test_add_photometry_from_raw_labview(dummy_logger):
     green_z_scored_dFF = np.array(nwbfile.acquisition["z_scored_green_dFF"].data)
     green_z_scored_dFF_reference = reference_dataframe["green"].values
 
-    # In our former MATLAB photometry preprocessing pipeline (used to create the reference dataframe), 
+    # In our former MATLAB photometry preprocessing pipeline (used to create the reference dataframe),
     # we would sometimes remove some samples from the beginning of our signals for alignment with behavior.
     # In this new pipeline, we do this alignment later so this is no longer necessary at this step.
     # This results in the signals in our created dict being longer than the reference signals in the dataframe,
@@ -282,12 +304,17 @@ def test_add_photometry_from_pyphotometry(dummy_logger):
     metadata = {}
     metadata["photometry"] = {}
     metadata["photometry"]["ppd_file_path"] = test_data_dir / "Lhem_barswitch_GACh4h_rDA3m_CKTL-2024-11-06-185407.ppd"
+    metadata["photometry"]["excitation_sources"] = [
+        "Doric Purple LED",
+        "Doric Blue LED",
+        "Doric Green LED",
+    ]
     add_dummy_photometry_metadata_to_metadata(metadata)
 
     # Define paths to reference data
     reference_data_path = test_data_dir / "sampleframe.csv"
     reference_dataframe = pd.read_csv(reference_data_path)
-    
+
     # Create a test NWBFile
     nwbfile = NWBFile(
         session_description="Mock session",
@@ -299,7 +326,7 @@ def test_add_photometry_from_pyphotometry(dummy_logger):
     photometry_data_dict = add_photometry(nwbfile=nwbfile, metadata=metadata, logger=dummy_logger)
 
     # Define the FiberPhotometryResponseSeries we expect to have been added to the nwbfile
-    expected_photometry_series = {"raw_470", "z_scored_470", "raw_405", "zscored_405", "raw_565", 
+    expected_photometry_series = {"raw_470", "z_scored_470", "raw_405", "zscored_405", "raw_565",
                                   "zscored_565", "raw_470_405_ratio", "zscored_470_405_ratio"}
     expected_sampling_rate = 86 # Hz
 
@@ -332,18 +359,18 @@ def test_add_photometry_from_pyphotometry(dummy_logger):
             f"{series_name} has a sampling rate of {getattr(nwbfile.acquisition[series_name], 'rate', None)}, "
             f"expected {expected_sampling_rate}"
         )
-        
+
     # Check that the photometry series in the nwbfile match the expected signals from the reference dataframe
-    nwb_signal_names = ["raw_470", "z_scored_470", "raw_405", "zscored_405", 
+    nwb_signal_names = ["raw_470", "z_scored_470", "raw_405", "zscored_405",
                         "raw_565", "zscored_565", "raw_470_405_ratio", "zscored_470_405_ratio"]
-    reference_signal_names = ["raw_green", "green_z_scored", "raw_405", "z_scored_405", 
+    reference_signal_names = ["raw_green", "green_z_scored", "raw_405", "z_scored_405",
                               "raw_red", "red_z_scored", "raw 470/405", "ratio_z_scored"]
 
     # Compare each signal in the nwbfile to its respective reference
     for sig_name, ref_name in zip(nwb_signal_names, reference_signal_names):
         signal = np.array(nwbfile.acquisition[sig_name].data)
         reference = reference_dataframe[ref_name].values
-    
+
         # Check that the lengths match
         assert len(signal) == len(reference), (
             f"Data length mismatch: {sig_name} has {len(signal)} points, "
@@ -368,16 +395,7 @@ def test_add_photometry_metadata(dummy_logger):
     # Create a test metadata dictionary
     metadata = {}
     metadata["photometry"] = {}
-    metadata["photometry"]["excitation_sources"] = [
-        "Thorlabs Purple LED",
-        "Thorlabs Blue LED",
-    ]
-    metadata["photometry"]["optic_fibers"] = [
-        "Doric 0.66mm Flat 40mm Optic Fiber",
-    ]
-    metadata["photometry"]["photodetectors"] = [
-        "Doric iFMC7-G2 (7 ports Fluorescence Mini Cube - Three Fluorophores)",
-    ]
+    add_dummy_photometry_metadata_to_metadata(metadata)
 
     # Create a test NWBFile
     nwbfile = NWBFile(
@@ -402,17 +420,20 @@ def test_add_photometry_metadata(dummy_logger):
     assert nwbfile.devices["Thorlabs Purple LED"].illumination_type == "LED"
     assert nwbfile.devices["Thorlabs Purple LED"].manufacturer == "Thorlabs"
     assert nwbfile.devices["Thorlabs Purple LED"].model == "M405FP1"
+
     assert "Thorlabs Blue LED" in nwbfile.devices
     assert nwbfile.devices["Thorlabs Blue LED"].excitation_wavelength_in_nm == 470.0
     assert nwbfile.devices["Thorlabs Blue LED"].illumination_type == "LED"
     assert nwbfile.devices["Thorlabs Blue LED"].manufacturer == "Thorlabs"
     assert nwbfile.devices["Thorlabs Blue LED"].model == "M470F3"
+
     assert "Doric 0.66mm Flat 40mm Optic Fiber" in nwbfile.devices
     optic_fiber = nwbfile.devices["Doric 0.66mm Flat 40mm Optic Fiber"]
     assert optic_fiber.numerical_aperture == 0.66
     assert optic_fiber.core_diameter_in_um == 200.0
     assert optic_fiber.manufacturer == "Doric"
     assert optic_fiber.model == "MFC_200/250-0.66_40mm_MF2.5_FLT"
+
     assert "Doric iFMC7-G2 (7 ports Fluorescence Mini Cube - Three Fluorophores)" in nwbfile.devices
     photodetector = nwbfile.devices["Doric iFMC7-G2 (7 ports Fluorescence Mini Cube - Three Fluorophores)"]
     assert photodetector.manufacturer == "Doric"
@@ -420,47 +441,49 @@ def test_add_photometry_metadata(dummy_logger):
     assert photodetector.detector_type == "Silicon photodiode"
     assert photodetector.detected_wavelength_in_nm == 960.0
 
+    # TODO add more checks
+
 
 def test_add_photometry_with_incomplete_metadata(capsys, dummy_logger):
     """
     Test that the add_photometry function responds appropriately to missing or incomplete metadata.
-    
-    If no 'photometry' key is in the metadata dictionary, it should print that we are skipping 
+
+    If no 'photometry' key is in the metadata dictionary, it should print that we are skipping
     photometry conversion and move on without raising any errors.
-    
+
     If there is a 'photometry' key in the metadata dict but the required paths to photometry data
     are not present, raise a ValueError telling the user which keys must be present in the dict.
     """
-    
+
     # Create a test metadata dictionary with no photometry key
     metadata = {}
-    
+
     # Create a test NWBFile
     nwbfile = NWBFile(
         session_description="Mock session",
         session_start_time=datetime.now(tz.tzlocal()),
         identifier="mock_session",
     )
-    
+
     # If we call the add_photometry function with no 'photometry' key in metadata,
     # It should print that we are skipping photometry conversion and return None for visits.
-    # This should not raise any errors, as omitting the 'photometry' key is a 
+    # This should not raise any errors, as omitting the 'photometry' key is a
     # valid way to specify that we have no photometry data for this session.
-    
+
     # Call the add_photometry function with no 'photometry' key in metadata
     photometry_data_dict = add_photometry(nwbfile=nwbfile, metadata=metadata, logger=dummy_logger)
     captured = capsys.readouterr() # capture stdout
-    
+
     # Check that the correct message was printed to stdout and returned dict is empty
     assert "No photometry metadata found for this session. Skipping photometry conversion." in captured.out
     assert photometry_data_dict.get('sampling_rate') is None
     assert photometry_data_dict.get('port_visits') is None
     assert photometry_data_dict.get('photometry_start') is None
-    
+
     # Create a test metadata dictionary with a photometry field and metadata but no photometry data
     metadata["photometry"] = {}
     add_dummy_photometry_metadata_to_metadata(metadata)
-    
+
     # Check that add_photometry raises a ValueError about missing fields in the metadata dictionary
     try:
         photometry_data_dict = add_photometry(nwbfile=nwbfile, metadata=metadata, logger=dummy_logger)
