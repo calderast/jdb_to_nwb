762699d5ba44eb72022638457d7fec480d807eff
Stephanie Crater <sacrater2017@gmail.com>
2025-07-31 16:24:49 -0700

    Update plot and point to correct resources dir

--- src/jdb_to_nwb/plotting/plot_ephys.py
+++ src/jdb_to_nwb/plotting/plot_ephys.py
@@ -3,35 +3,32 @@ import pandas as pd
 import os
 
 
-def plot_channel_map(probe_name, channel_geometry, fig_dir=None):
+def plot_channel_map(probe_name, channel_coords, fig_dir=None):
     """
     Plot the channel map with channel numbers at their corresponding coordinates.
 
     Parameters:
         probe_name (str): 
             The name of the probe.
-        channel_geometry (pd.DataFrame): 
-            The channel geometry with columns 'x' and 'y'.
+        channel_coords (pd.DataFrame): 
+            Dataframe with columns 'x_um', 'y_um', and 'intan_channel'.
         fig_dir (str): 
             The directory to save the figure. If None, the figure will not be saved.
     """
-    plt.figure(figsize=(10, 6))
-    
-    # First establish the plot limits using scatter (otherwise the text labels will take forever to draw)
-    plt.scatter(channel_geometry['x'], channel_geometry['y'], alpha=0)  # invisible points to set boundaries
-    
-    # Then plot the text labels
-    for channel_num in channel_geometry.index:
-        x = channel_geometry.iloc[channel_num]['x']
-        y = channel_geometry.iloc[channel_num]['y']
-        plt.text(x, y, str(channel_num), fontsize=10, ha='center', va='center')
+    plt.figure(figsize=(16, 6))
+    plt.scatter(channel_coords["x_um"], channel_coords["y_um"], s=10, alpha=0)
+    for _, row in channel_coords.iterrows():
+        plt.text(row["x_um"], row["y_um"], row["intan_channel"], fontsize=8, ha='center', va='center')
 
     plt.title(f'{probe_name} Channel Map')
+    plt.xlim(min(channel_coords["x_um"]) - 100, max(channel_coords["x_um"]) + 100)
+    plt.ylim(min(channel_coords["y_um"]) - 100, max(channel_coords["y_um"]) + 100)
     plt.xlabel('X Position (μm)')
     plt.ylabel('Y Position (μm)')
 
     if fig_dir:
-        save_path = os.path.join(fig_dir, "channel_coords.png")
+        save_name = f"{probe_name.lower().replace(' ', '_').replace(',', '').replace('-','')}"
+        save_path = os.path.join(fig_dir, f"{save_name}_channel_coords.png")
         plt.savefig(save_path, dpi=300, bbox_inches="tight")
         plt.close()
 
