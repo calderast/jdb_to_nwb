c28de02069acbd83feb8a70917b0d945cf168cb5
Stephanie Crater <stephcrater@berkeley.edu>
2026-05-11 23:23:51 -0400

    Try to align when we have more trials than ground truth visits

--- src/jdb_to_nwb/convert_behavior.py
+++ src/jdb_to_nwb/convert_behavior.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from pynwb import NWBFile
 from hdmf.common.table import DynamicTable, VectorData
 from ndx_franklab_novela import AssociatedFiles
-from .timestamps_alignment import trim_sync_pulses, handle_timestamps_reset
+from .timestamps_alignment import trim_sync_pulses, align_via_interpolation, handle_timestamps_reset
 from .plotting.plot_behavior import plot_maze_configurations, plot_trial_time_histogram
 
 
@@ -296,12 +296,32 @@ def align_data_to_visits(trial_data, block_data, metadata, logger):
         ground_truth_visit_times, arduino_visits = trim_sync_pulses(ground_truth_visit_times, arduino_visits, logger)
 
     # We should never have more trials than photometry/ephys visits.
-    # If we do, error so we can figure out why this happened and handle it accordingly.
+    # If we do, it likely means photometry/ephys crashed while behavior was still running (see Github issue #194)
+    # This is not ideal for alignment but we will try our best. 
     elif len(trial_data) > len(ground_truth_visit_times):
-        logger.critical(f"Found more trials recorded by arduino ({len(trial_data)}) "
-                        f"than {ground_truth_time_source} visits ({len(ground_truth_visit_times)})!!!")
-        logger.critical("This should never happen!!! Skipping alignment of trial/block data.")
-        return trial_data, block_data
+        arduino_visits = [trial['beam_break_start'] for trial in trial_data]
+        n_extra = len(trial_data) - len(ground_truth_visit_times)
+        # Log it at CRITICAL and complain a lot so the user knows something is wrong!
+        logger.critical(
+            f"Found {n_extra} more trial(s) recorded by arduino ({len(trial_data)}) than "
+            f"ground truth {ground_truth_time_source} visits ({len(ground_truth_visit_times)}). "
+        )
+        logger.critical(f"This should not happen and likely means that {ground_truth_time_source} crashed!!")
+        logger.critical("Attempting alignment by building interpolation function from "
+                       f"{len(ground_truth_visit_times)} matched {ground_truth_time_source} visits "
+                       f"and applying to all {len(trial_data)} trials (unmatched trial(s) will be extrapolated).")
+        logger.critical("Carefully check DEBUG conversion log and all created figures to make sure alignment is ok.")
+        
+        # TODO: create extra "ground truth" visit time(s) via extrapolation
+        # so we have the same number of visit times and ground truth visit times
+        # this gets risky the more we are extrapolating, so log everything.
+        # make sure we don't mess with any of the exisiting ground truth visit times
+        ground_truth_visit_times = align_via_interpolation(
+            unaligned_timestamps=arduino_visits,
+            unaligned_visit_times=arduino_visits,
+            ground_truth_visit_times=ground_truth_visit_times,
+            logger=logger,
+        )
 
     # Now that we have the correct number of ground truth visit times, replace arduino times with ground truth times
     for trial, visit_time in zip(trial_data, ground_truth_visit_times):
